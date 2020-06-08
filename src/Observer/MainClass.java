9e3b9db92bbf708e5d873290199069e6c368b487
weronikakurczyna <56171356+weronikakurczyna@users.noreply.github.com>
2020-06-08 23:58:51 +0200

    Some changes.

--- src/Observer/MainClass.java
+++ src/Observer/MainClass.java
@@ -1,8 +1,11 @@
 package Observer;
 
 public class MainClass {
+    public static void main(String[] args) {
+        TemperatureSensor ts = new TemperatureSensor(10.2);
+        CelsiusDisplay cd = new CelsiusDisplay(ts);
+        ts.addObserver(cd);
+        ts.setTemperatureCelsius(3.4);
+    }
 
-    TemperatureSensor ts = new TemperatureSensor(10.2);
-    CelsiusDisplay cd = new CelsiusDisplay(ts);
-    
 }
